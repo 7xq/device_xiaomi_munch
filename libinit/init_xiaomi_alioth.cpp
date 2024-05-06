7d5b229a7a23fad9edbf946dad7c2d1543709260
Shaquib Imdad <mail@shaquib.dev>
2024-05-07 01:47:27 +0530

    Initial: Lineage bringup for munch

--- libinit/init_xiaomi_alioth.cpp
+++ libinit/init_xiaomi_alioth.cpp
@@ -9,49 +9,49 @@
 
 #include "vendor_init.h"
 
-static const variant_info_t aliothin_info = {
+static const variant_info_t munchin_info = {
     .hwc_value = "INDIA",
     .sku_value = "",
 
-    .brand = "Mi",
-    .device = "aliothin",
-    .marketname = "Mi 11X",
-    .model = "M2012K11AI",
+    .brand = "POCO",
+    .device = "munchin",
+    .marketname = "POCO F4",
+    .model = "22021211RG",
     .build_fingerprint = "Mi/aliothin/aliothin:13/TKQ1.220829.002/V14.0.5.0.TKHINXM:user/release-keys",
 
-    .nfc = false,
+    .nfc = true,
 };
 
-static const variant_info_t alioth_global_info = {
+static const variant_info_t munch_global_info = {
     .hwc_value = "GLOBAL",
     .sku_value = "",
 
     .brand = "POCO",
-    .device = "alioth",
-    .marketname = "POCO F3",
-    .model = "M2012K11AG",
+    .device = "munch",
+    .marketname = "POCO F4",
+    .model = "22021211RG",
     .build_fingerprint = "POCO/alioth_global/alioth:13/TKQ1.220829.002/V14.0.8.0.TKHMIXM:user/release-keys",
 
     .nfc = true,
 };
 
-static const variant_info_t alioth_info = {
+static const variant_info_t munch_info = {
     .hwc_value = "",
     .sku_value = "",
 
     .brand = "Redmi",
-    .device = "alioth",
-    .marketname = "Redmi K40",
-    .model = "M2012K11AC",
+    .device = "munch",
+    .marketname = "Redmi K40s",
+    .model = "22021211RC",
     .build_fingerprint = "Redmi/alioth/alioth:13/TKQ1.220829.002/V14.0.8.0.TKHCNXM:user/release-keys",
 
     .nfc = true,
 };
 
 static const std::vector<variant_info_t> variants = {
-    aliothin_info,
-    alioth_global_info,
-    alioth_info,
+    munchin_info,
+    munch_global_info,
+    munch_info,
 };
 
 void vendor_load_properties() {
--- lineage_munch.mk
+++ lineage_munch.mk
@@ -11,14 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from alioth device
-$(call inherit-product, device/xiaomi/alioth/device.mk)
+# Inherit from munch device
+$(call inherit-product, device/xiaomi/munch/device.mk)
 
-PRODUCT_NAME := lineage_alioth
-PRODUCT_DEVICE := alioth
+PRODUCT_NAME := lineage_munch
+PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO F3
+PRODUCT_MODEL := POCO F4
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
